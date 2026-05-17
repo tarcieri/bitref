89c975bf510b7effd86cc1d62ba2934e98ae23ad
Tony Arcieri <bascule@gmail.com>
2026-05-16 19:45:39 -0600

    Update push branch name

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 #![no_std]
 #![doc = include_str!("../README.md")]
-#![doc(
-    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg"
-)]
 
 mod errors;
 mod iter;
